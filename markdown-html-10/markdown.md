987972cbc8ca02b62d5257502e0af02d765144ff
MaverickPoly <realahmadjon82@gmail.com>
2025-08-25 20:00:53 +0500

    Markdown to html

---
+++ markdown-html-10/markdown.md
@@ -0,0 +1,34 @@
+# Altair Tech
+
+## Products
+
+- Altair Cloud - Secure Cloud Storage for Files.
+- Altair Edu - Learning platform for students.
+- Altair Code - Programmer's learning hub.
+- Altair Chat - Telegram-like chat application.
+- Altair News - News Articles.
+- Altair Games - Games to be played.
+- Altair Blog - twitter-like platform for blogging.
+- Altair Assistant - Ai Chatbot
+
+Those are some of the programs that I am going to have in my tier.
+
+They are going to be implemented in various **programming languages** and *technologies**
+
+
+This is my sample python code:
+
+```python
+import tkinter
+
+# This is a sample python tkinter app
+window = tkinter.Tk()
+
+window.mainloop()
+```
+
+---
+
+Hope you enjoyed!
+
+;)
---
+++ markdown-html-10/src/main.rs
@@ -0,0 +1,33 @@
+use pulldown_cmark::{Options, Parser, html};
+use std::env;
+use std::fs;
+use std::io::Write;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: cargo run <markdown_file> <html_file>");
+        return;
+    }
+
+    let markdown_file = &args[1];
+    let html_file = &args[2];
+
+    let markdown_content = fs::read_to_string(markdown_file).expect("Invalid markdown file!");
+
+    let options = Options::empty();
+    let parser = Parser::new_ext(&markdown_content, options);
+
+    let mut html_content = String::new();
+    html::push_html(&mut html_content, parser);
+
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(html_file)
+        .expect("Failed to open html file!");
+    file.write_all(html_content.as_bytes())
+        .expect("Failed to write html to file!");
+}
