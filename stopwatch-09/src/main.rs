6c8374e0db4e1bc4850f6af4c4a4d5760fc99f56
MaverickPoly <realahmadjon82@gmail.com>
2025-08-27 11:57:32 +0500

    Stopwatch CLI

---
+++ stopwatch-09/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+use std::thread;
+use std::time;
+
+fn main() {
+    print!("Enter number of seconds: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input: u32 = input.trim().parse().expect("Invalid input!");
+
+    println!("Stopwatch started!");
+
+    for i in 0..input {
+        println!("{}", input - i);
+        thread::sleep(time::Duration::from_secs(1));
+    }
+    println!("Stopwatch stopped!");
+}
